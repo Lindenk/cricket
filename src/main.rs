4ab51428d9c8e99d5edb13bfce3a1329d5868730
Daniel Thompson <danthompson41@gmail.com>
2022-04-29 22:38:20 -0400

    Making the demo more complicated

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn spawn(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // The ground
-    let size = Vec2::new(1000.0, 50.0);
+    let size = Vec2::new(1000.0, 20.0);
     commands
         // Spawn a bundle that contains at least a `GlobalTransform`
         .spawn_bundle(SpriteBundle {
@@ -30,7 +30,31 @@ fn spawn(mut commands: Commands) {
                 custom_size: Some(size),
                 ..Default::default()
             },
-            transform: Transform::from_translation(Vec3::new(0.0, -300.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(0.0, -200.0, 0.0)),
+            ..Default::default()
+        })
+        // Make it a rigid body
+        .insert(RigidBody::Static)
+        // Attach a collision shape
+        .insert(CollisionShape::Cuboid {
+            half_extends: size.extend(0.0) / 2.0,
+            border_radius: None,
+        })
+        // Define restitution (so that it bounces)
+        .insert(PhysicMaterial {
+            restitution: 1.5,
+            ..Default::default()
+        });
+
+    commands
+        // Spawn a bundle that contains at least a `GlobalTransform`
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::WHITE,
+                custom_size: Some(size),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 200.0, 0.0)),
             ..Default::default()
         })
         // Make it a rigid body
@@ -45,7 +69,6 @@ fn spawn(mut commands: Commands) {
             restitution: 0.5,
             ..Default::default()
         });
-
     // The Ball
     let size = Vec2::new(30.0, 30.0);
     commands
@@ -56,7 +79,7 @@ fn spawn(mut commands: Commands) {
                 custom_size: Some(size),
                 ..Default::default()
             },
-            transform: Transform::from_translation(Vec3::new(-400.0, 200.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(-400.0, 0.0, 0.0)),
             ..Default::default()
         })
         // Make it a rigid body
@@ -67,10 +90,10 @@ fn spawn(mut commands: Commands) {
             border_radius: None,
         })
         // Add an initial velocity. (it is also possible to read/mutate this component later)
-        .insert(Velocity::from(Vec2::X * 300.0).with_angular(AxisAngle::new(Vec3::Z, -PI)))
+        .insert(Velocity::from(Vec2::X * 200.0).with_angular(AxisAngle::new(Vec3::Z, -PI)))
         // Define restitution (so that it bounces)
         .insert(PhysicMaterial {
-            restitution: 0.7,
+            restitution: 1.2,
             ..Default::default()
         });
 }
